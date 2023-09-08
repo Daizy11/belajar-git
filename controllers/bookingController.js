1c773689494b22ee311a6b28851dff909c171fb9
Daizy11 <habib.erdian@gmail.com>
2023-09-08 17:07:37 +0700

    updated bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -28,7 +28,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://127.0.0.1:3000/img/tours/${tour.imageCover}`],
           },
         },
       },
